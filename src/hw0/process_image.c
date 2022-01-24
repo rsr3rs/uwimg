fc38896b3fdeb6696586da84a2456d1142e5af4f
Robin <yangy87@uw.edu>
2022-01-23 17:14:00 -0800

    update

--- src/hw0/process_image.c
+++ src/hw0/process_image.c
@@ -1,24 +1,64 @@
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
+#include <stdlib.h>
 #include <math.h>
 #include "image.h"
 
 float get_pixel(image im, int x, int y, int c)
 {
     // TODO Fill this in
-    return 0;
+    if (x < 0) {
+        x = 0;
+    }
+    if (x >= im.w) {
+        x = im.w - 1;
+    }
+    if (y < 0) {
+        y = 0;
+    }
+    if (y >= im.h) {
+        y = im.h - 1;
+    }
+    if (c < 0) {
+        c = 0;
+    }
+    if (c >= im.c) {
+        c = im.c - 1;
+    }
+    return im.data[x + y * im.w + c * im.w * im.h];
+    // return 0;
 }
 
 void set_pixel(image im, int x, int y, int c, float v)
 {
     // TODO Fill this in
+    if (x < 0) {
+        return;
+    }
+    if (x >= im.w) {
+        return;
+    }
+    if (y < 0) {
+        return;
+    }
+    if (y >= im.h) {
+        return;
+    }
+    if (c < 0) {
+        return;
+    }
+    if (c >= im.c) {
+        return;
+    }
+    im.data[x + y * im.w + c * im.w * im.h] = v;
 }
 
 image copy_image(image im)
 {
     image copy = make_image(im.w, im.h, im.c);
     // TODO Fill this in
+    memcpy(copy.data, im.data, sizeof(float)*im.w * im.h * im.c);
     return copy;
 }
 
@@ -27,17 +67,41 @@ image rgb_to_grayscale(image im)
     assert(im.c == 3);
     image gray = make_image(im.w, im.h, 1);
     // TODO Fill this in
+    for (int i = 0; i < im.w; i++) {
+        for (int j = 0; j < im.h; j++) {
+            gray.data[i + j * im.w] = 0.299 * im.data[i + j * im.w + 0] +
+                0.587 * im.data[i + j * im.w + im.h * im.w] +
+                0.114 * im.data[i + j * im.w + im.h * im.w * 2];
+        }
+    }
     return gray;
 }
 
 void shift_image(image im, int c, float v)
 {
     // TODO Fill this in
+    for (int i = 0; i < im.w; i++) {
+        for (int j = 0; j < im.h; j++) {
+            im.data[i + j * im.w + im.h * im.w * c] += v;
+        }
+    }
 }
 
 void clamp_image(image im)
 {
     // TODO Fill this in
+    for (int i = 0; i < im.w; i++) {
+        for (int j = 0; j < im.h; j++) {
+            for (int k = 0; k < im.c; k++) {
+                if (im.data[i + j * im.w + im.h * im.w * k] < 0.0) {
+                    im.data[i + j * im.w + im.h * im.w * k] = 0;
+                }
+                if (im.data[i + j * im.w + im.h * im.w * k] > 1.0) {
+                    im.data[i + j * im.w + im.h * im.w * k] = 1;
+                }
+            }
+        }
+    }
 }
 
 
@@ -55,9 +119,96 @@ float three_way_min(float a, float b, float c)
 void rgb_to_hsv(image im)
 {
     // TODO Fill this in
+    for (int i = 0; i < im.w; i++) {
+        for (int j = 0; j < im.h; j++) {
+            float R = im.data[i + j * im.w];
+            float G = im.data[i + j * im.w + im.h * im.w];
+            float B = im.data[i + j * im.w + im.h * im.w * 2];
+            float V = three_way_max(R, G, B);
+            float m = three_way_min(R, G, B);
+            float C = V - m;
+            float S = 0;
+            if (V != 0) {
+                S = C / V;
+            }
+            float H = 0;
+            if (C == 0) {
+                H = 0;
+            } else if (V == R) {
+                H = (G - B) / C;
+            } else if (V == G) {
+                H = (B - R) / C + 2;
+            } else if (V == B) {
+                H = (R - G) / C + 4;
+            }
+            if (H < 0) {
+                H = H / 6 + 1;
+            } else {
+                H = H / 6;
+            }
+            im.data[i + j * im.w] = H;
+            im.data[i + j * im.w + im.h * im.w] = S;
+            im.data[i + j * im.w + im.h * im.w * 2] = V;
+        }
+    }
 }
 
 void hsv_to_rgb(image im)
 {
     // TODO Fill this in
+    for (int i = 0; i < im.w; i++) {
+        for (int j = 0; j < im.h; j++) {
+            double r, g, b, p, q, t, x;
+            float h = im.data[i + j * im.w];
+            float s = im.data[i + j * im.w + im.h * im.w];
+            float v = im.data[i + j * im.w + im.h * im.w * 2];
+            if (s <= 0.0) {
+                r = v;
+                g = v;
+                b = v;
+            } else {
+                int hi = (int)(h * 6);
+                x = h * 6 - hi;
+                p = v * (1.0 - s);
+                q = v * (1.0 - (s * x));
+                t = v * (1.0 - (s * (1.0 - x)));
+                if (0 == hi && hi < 1) {
+                    r = v;
+                    g = t;
+                    b = p;
+                } else if (1 == hi && hi < 2) {
+                    r = q;
+                    g = v;
+                    b = p;
+                } else if (2 <= hi && hi < 3) {
+                    r = p;
+                    g = v;
+                    b = t;
+                } else if (3 <= hi && hi < 4) {
+                    r = p;
+                    g = q;
+                    b = v;
+                } else if (4 <= hi && hi < 5) {
+                    r = t;
+                    g = p;
+                    b = v;
+                } else {
+                    r = v;
+                    g = p;
+                    b = q;
+                }
+            }
+            im.data[i + j * im.w] = r;
+            im.data[i + j * im.w + im.h * im.w] = g;
+            im.data[i + j * im.w + im.h * im.w * 2] = b;
+        }
+    }
+}
+
+void scale_image(image im, int c, float v) {
+    for (int i = 0; i < im.w; i++) {
+        for (int j = 0; j < im.h; j++) {
+            im.data[i + j * im.w + im.h * im.w * c] *= v;
+        }
+    }
 }
--- src/hw1/resize_image.c
+++ src/hw1/resize_image.c
@@ -4,24 +4,63 @@
 float nn_interpolate(image im, float x, float y, int c)
 {
     // TODO Fill in
-    return 0;
+    // return 0;
+    return get_pixel(im, roundf(x), roundf(y), c);
 }
 
 image nn_resize(image im, int w, int h)
 {
     // TODO Fill in (also fix that first line)
-    return make_image(1,1,1);
+    // return make_image(1,1,1);
+    image new_img = make_image(w, h, im.c);
+    float ax = (float)im.w / w;
+    float bx = -0.5 + 0.5 * ax;
+    float ay = (float)im.h / h;
+    float by = -0.5 + 0.5 * ay;
+    for (int i = 0; i < im.c; i++) {
+        for (int j = 0; j < w; j++) {
+            for (int k = 0; k < h; k++) {
+                float x = (float)ax * j + bx;
+                float y = (float)ay * k + by;
+                set_pixel(new_img, j, k, i, nn_interpolate(im, x, y, i));
+            }
+        }
+    }
+    return new_img;
 }
 
 float bilinear_interpolate(image im, float x, float y, int c)
 {
     // TODO
-    return 0;
+    // return 0;
+    float v1 = get_pixel(im, floorf(x), floorf(y), c);
+    float v2 = get_pixel(im, ceilf(x), floorf(y), c);
+    float v3 = get_pixel(im, floorf(x), ceilf(y), c);
+    float v4 = get_pixel(im, ceilf(x), ceilf(y), c);
+    float q1 = v2 * (x - floorf(x)) + v1 * (ceilf(x) - x);
+    float q2 = v4 * (x - floorf(x)) + v3 * (ceilf(x) - x);
+    float q = q2 * (y - floorf(y)) + q1 * (ceilf(y) - y);
+    return q;
 }
 
 image bilinear_resize(image im, int w, int h)
 {
     // TODO
-    return make_image(1,1,1);
+    // return make_image(1,1,1);
+    image new_img = make_image(w, h, im.c);
+    float ax = (float)im.w / w;
+    float bx = -0.5 + 0.5 * ax;
+    float ay = (float)im.h / h;
+    float by = -0.5 + 0.5 * ay;
+    for (int i = 0; i < im.c; i++) {
+        for (int j = 0; j < w; j++) {
+            for (int k = 0; k < h; k++) {
+                float x = (float)ax * j + bx;
+                float y = (float)ay * k + by;
+                set_pixel(new_img, j, k, i, bilinear_interpolate(im, x, y, i));
+            }
+        }
+    }
+    return new_img;
 }
 
